0de945778ec384d422dd7dc2df01f8de5444192a
Vida Komaria <71854583+vidakomaria@users.noreply.github.com>
2022-06-21 19:54:45 +0700

    Revert "Add feature "User Profile""

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,34 @@
+import '../styles/Login.css'
+import { Button, Input, Image } from "@chakra-ui/react";
+import logo from '../assets/logo-dark.png'
+import { Link } from 'react-router-dom';
+
+export default function Login(){
+    return(
+        <div className="container">
+            <div className='left'>
+                <div className='welcome'>
+                    Welcome Back
+                </div>
+
+                <div className='inputLogin'>
+                    <p>Username</p>
+                    <Input placeholder='Enter Your Username'/>
+                    <p>Password</p>
+                    <Input placeholder='Password' type='password'/>
+                </div>
+
+                <div className='btnLogin'>
+                    <Link to={'daftar-buku'}>
+                        <Button colorScheme={'#112B3C'}>Login</Button>
+                    </Link>
+                </div>
+            </div>
+
+            <div className='right'>
+                BukuKita
+                <img src={logo}/>
+            </div>
+        </div>
+    )
+}
