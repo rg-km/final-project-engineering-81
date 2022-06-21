3e5cdca9745729bedd8cf82854a419056723b661
Rizky Ananda <rizkyananda007@gmail.com>
2022-06-21 21:20:41 +0800

    Comment some error code & Add some package (formik, react-icons)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,7 +6,7 @@ import { Routes, Route, useLocation } from 'react-router-dom'
 import BookDetail from './components/BookDetail';
 import Footer from './components/Footer';
 import Cart from './components/Cart';
-import Checkout from './components/Checkout';
+// import Checkout from './components/Checkout';
 
 function App() {
   const loc = useLocation()
@@ -37,7 +37,7 @@ function App() {
         <Route path='keranjang' element={ <Cart/> }/>
 
         {/* CHECKOUT */}
-        <Route path='checkout' element={ <Checkout/> }/>
+        {/* <Route path='checkout' element={ <Checkout/> }/> */}
 
       </Routes>
 
@@ -53,5 +53,3 @@ function App() {
 }
 
 export default App;
-
-{/* <Link to={`/star-wars/movies/${getID(movie.url)}`} key={index}></Link> */}
