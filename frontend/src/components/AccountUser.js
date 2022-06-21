2a0b8703e60f45bab8c390e9998b93332f5c635e
Rizky Ananda <rizkyananda007@gmail.com>
2022-06-21 19:01:47 +0800

    Add content for Biodata & Akun Saya. Separate Biodata from Akun Saya component

--- frontend/src/components/AccountUser.js
+++ frontend/src/components/AccountUser.js
@@ -3,6 +3,7 @@ import { Heading, Box, Stack, SimpleGrid, Container } from '@chakra-ui/react'
 
 import contentAkunUser from '../subComponents/contentAkunUser'
 import contentPesananUser from '../subComponents/contentPesananUser'
+import contentDataDiriUser from '../subComponents/contentDataDiriUser'
 import Navbar from './Navbar'
 import Footer from './Footer'
 
@@ -10,6 +11,7 @@ import Footer from './Footer'
 const FeatureContent = ({ content, ...rest }) => {
     return (
       <Box p={8} shadow='md' borderWidth='1px' {...rest}>
+        {content === 'Data Diri' ? contentDataDiriUser() : null}
         {content === 'Akun Saya' ? contentAkunUser() : null}
         {content === 'Pesanan Saya' ? contentPesananUser() : null}
       </Box>
@@ -28,6 +30,22 @@ const AccountUser = () => {
             <Container maxW='container.xl' mt={100}>
             <Stack spacing={8} direction='row' >
                 <Box w='20%' h='300' p={8} shadow='md' borderWidth='1px'>
+                    <Box 
+                    as='button'
+                    mb={3}
+                    color= {contentActive === 'Data Diri' ? '#1DC9A1' : '#000'} 
+                    fontSize='xl'
+                    fontWeight='semibold' 
+                    _hover={{ color: '#1DC9A1' }} 
+                    _active={{ color: '#1DC9A1', transform: 'scale(0.9)' }}
+                    _focus={{ color: '#1DC9A1'}}
+                    onClick={ () => setcontentActive('Data Diri') }
+                    >
+                        Data Diri
+        
+                    </Box>
+                    <br/>
+
                     <Box 
                     as='button'
                     mb={3}
