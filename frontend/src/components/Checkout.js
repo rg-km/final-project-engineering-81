0cd7911c770b80419d1a976038b321b7a97aa830
Vida <vidakomaria47@gmail.com>
2022-06-22 20:33:42 +0700

    add : UI form bukti pembayaran

--- frontend/src/components/Checkout.js
+++ frontend/src/components/Checkout.js
@@ -1,8 +1,67 @@
-import { Button } from '@chakra-ui/react'
+import { Button, Input, useDisclosure } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import '../styles/Checkout.css'
+import {
+    Modal,
+    ModalOverlay,
+    ModalContent,
+    ModalHeader,
+    ModalFooter,
+    ModalBody,
+    ModalCloseButton,
+  } from '@chakra-ui/react'
 
 export default function Checkout(){
+    function Payment() {
+        const { isOpen, onOpen, onClose } = useDisclosure()
+
+        return (
+            <>
+            <Button onClick={onOpen}>Bayar Pesanan</Button>
+
+            <Modal closeOnOverlayClick={false} isOpen={isOpen} onClose={onClose}>
+                <ModalOverlay />
+                <ModalContent className='modal'>
+                    <ModalHeader className='modal-header'>
+                        <h2>Selesaikan pembayaran sebelum</h2>
+                        <h1>1 Januari 2022, 23.59 WIB</h1>
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody pb={6} className='modal-body'>
+                        <div className='transfer'>
+                            <div className='bank-addres'>
+                                <img src='https://dinkes.dairikab.go.id/wp-content/uploads/sites/12/2022/03/default-img.gif'/>
+                                <div>
+                                    <h2><b>BANK NAMA_BANK</b></h2>
+                                    <h3>A/N BukuKita</h3>
+                                </div>
+                            </div>
+                            <Input value={1234567890} isReadOnly />
+                            <h2><b>Nominal Transfer</b></h2>
+                            <Input value={100000} isReadOnly />
+                        </div>
+
+                        <div className='bukti'>
+                            <h2><b>Bukti Pembayaran</b></h2>
+                            <div className='input-img'>
+                                <Input type={'image'} className='img-input' />
+                            </div>
+                            <Button>Upload Bukti Transfer</Button>
+                        </div>
+                    </ModalBody>
+
+                    <ModalFooter>
+                        {/* <Button colorScheme='blue' mr={3}>
+                        Save
+                        </Button> */}
+                        <Button onClick={onClose}>Cancel</Button>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
+            </>
+        )
+    }
+
     return(
         <div className="co-container">
             <div className="co-left">
@@ -80,9 +139,13 @@ export default function Checkout(){
                             <h1 className='green'><b>Rp. xx.000</b></h1>
                         </div>
                     </div>
-                    <Button>Bayar Pesanan</Button>
+                    <Payment/>
                 </div>
             </div>
+
+
+            {/* MODAL */}
+            
         </div>
     )
 }
