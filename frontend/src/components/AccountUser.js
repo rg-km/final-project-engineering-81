fa43760790ccccdbcfe4b561f72fe1846b130eed
Rizky Ananda <rizkyananda007@gmail.com>
2022-06-24 20:10:35 +0800

    Change position Pesanan Saya to top & Make asset img not dragable.

--- frontend/src/components/AccountUser.js
+++ frontend/src/components/AccountUser.js
@@ -26,6 +26,20 @@ const AccountUser = () => {
             <Container maxW='container.xl' mt={100}>
             <Stack spacing={8} direction='row' >
                 <Box w='20%' h='300' p={8} shadow='md' borderWidth='1px' textAlign='left'>
+                    <Box  
+                    as='button'
+                    mb={3}
+                    color= {contentActive === 'Pesanan Saya' ? '#1DC9A1' : '#000'} 
+                    fontSize='xl'
+                    fontWeight='semibold'
+                    _hover={{ color: '#1DC9A1' }} 
+                    _active={{ color: '#1DC9A1', transform: 'scale(0.9)' }}
+                    onClick={ () => setcontentActive('Pesanan Saya') }
+                    >
+                        Pesanan Saya
+                    </Box>
+                    <br/>
+
                     <Box 
                     as='button'
                     mb={3}
@@ -44,7 +58,7 @@ const AccountUser = () => {
 
                     <Box 
                     as='button'
-                    mb={3}
+                    mb={16}
                     color= {contentActive === 'Akun Saya' ? '#1DC9A1' : '#000'} 
                     fontSize='xl'
                     fontWeight='semibold' 
@@ -58,19 +72,7 @@ const AccountUser = () => {
                     </Box>
                     <br/>
 
-                    <Box  
-                    as='button'
-                    mb={16}
-                    color= {contentActive === 'Pesanan Saya' ? '#1DC9A1' : '#000'} 
-                    fontSize='xl'
-                    fontWeight='semibold'
-                    _hover={{ color: '#1DC9A1' }} 
-                    _active={{ color: '#1DC9A1', transform: 'scale(0.9)' }}
-                    onClick={ () => setcontentActive('Pesanan Saya') }
-                    >
-                        Pesanan Saya
-                    </Box>
-                    <br/>
+                    
                     
                     <Link to='/' replace={true}> 
                         <Box 
