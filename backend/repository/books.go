4486a5ee438b816340556bbdcff61b5a6734fd30
novelayesaya99 <novelaysytmb@gmail.com>
2022-06-24 19:54:51 +0700

    perbaikan api feature/booklist

--- backend/repository/books.go
+++ backend/repository/books.go
@@ -2,7 +2,6 @@ package repository
 
 import (
 	"database/sql"
-	"strconv"
 )
 
 type BookRepository struct {
@@ -13,76 +12,70 @@ func NewBookRepository(db *sql.DB) *BookRepository {
 	return &BookRepository{db: db}
 }
 
-func (u *BookRepository) LoadOrCreate() ([]Book, error) {
-	records, err := u.db.Load("books")
+func (u *BookRepository) FetchBookByID(id int64) (Book, error) {
+
+	var book Book
+	var sqlStatement string
+
+	sqlStatement = "SELECT * FROM books WHERE id = ?"
+
+	row, err := u.db.Query(sqlStatement, id)
+
 	if err != nil {
-		records = [][]string{
-			{"penulis", "judul", "harga"},
-		}
-		if err := u.db.Save("book", records); err != nil {
-			return nil, err
-		}
+		return book, err
 	}
 
-	result := make([]Book, 0)
-	for i := 1; i < len(records); i++ {
-		harga, err := strconv.Atoi(records[i][2])
-		if err != nil {
-			return nil, err
-		}
+	defer row.Close()
 
-		user := Book{
-			Penulis: records[i][0],
-			Judul:   records[i][1],
-			Harga:   harga,
+	for row.Next() {
+		if err := row.Scan(&book.ID, &book.Judul, &book.Penerbit, &book.TahunTerbit, &book.JumlahHalaman, &book.ISBN, &book.Kategori, &book.Bahasa, &book.Berat, &book.Harga, &book.Kondisi, &book.Deskripsi, &book.Stok); err != nil {
+			return book, err
 		}
-		result = append(result, user)
+	}
+	return book, nil
+
+}
+
+func (u *BookRepository) FetchBookByJudul(judul string) (Book, error) {
+
+	var book Book
+	var sqlStatement string
+
+	sqlStatement = "SELECT * FROM books WHERE judul = ?"
+
+	row := u.db.QueryRow(sqlStatement, judul)
+
+	err := row.Scan(&book.ID, &book.Judul, &book.Penerbit, &book.TahunTerbit, &book.JumlahHalaman, &book.ISBN, &book.Kategori, &book.Bahasa, &book.Berat, &book.Harga, &book.Kondisi, &book.Deskripsi, &book.Stok)
+
+	if err != nil {
+		return book, err
 	}
 
-	return result, nil
+	return book, nil
 
 }
 
 func (u *BookRepository) FetchBooks() ([]Book, error) {
-	books, stmt := []Book{}, `SELECT id, judul, penerbit, tahun_terbit, jumlah_halaman, isbn, kategori, bahasa, berat, harga, kondisi, deskripsi, stok`
 
-	rows, err := u.db.Query(stmt)
+	var books []Book
+	var sqlStatement string
+
+	sqlStatement = "SELECT * FROM books"
+
+	rows, err := u.db.Query(sqlStatement)
+
 	if err != nil {
 		return nil, err
 	}
 
 	defer rows.Close()
+
 	for rows.Next() {
 		var book Book
-		err = rows.Scan(
-			&book.ID,
-			&book.Judul,
-			&book.Penerbit,
-			&book.TahunTerbit,
-			&book.JumlahHalaman,
-			&book.ISBN,
-			&book.Kategori,
-			&book.Bahasa,
-			&book.Berat,
-			&book.Harga,
-			&book.Kondisi,
-			&book.Deskripsi,
-			&book.Stok,
-		)
-
-		if err != nil {
+		if err := rows.Scan(&book.ID, &book.Judul, &book.Penerbit, &book.TahunTerbit, &book.JumlahHalaman, &book.ISBN, &book.Kategori, &book.Bahasa, &book.Berat, &book.Harga, &book.Kondisi, &book.Deskripsi, &book.Stok); err != nil {
 			return nil, err
 		}
-
 		books = append(books, book)
 	}
 	return books, nil
-}
-
-func (u *BookRepository) SelectAll() ([]Book, error) {
-	bookItems, err := u.LoadOrCreate()
-	if err != nil {
-		return nil, err
-	}
-	return bookItems, nil
 }
