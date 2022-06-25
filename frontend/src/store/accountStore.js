0af964388834254638cf83064d5b05c7d451df72
Vida <vidakomaria47@gmail.com>
2022-06-25 20:52:45 +0700

    [FE] Zustand untuk menyimpan data account setelah login

--- frontend/src/store/accountStore.js
+++ frontend/src/store/accountStore.js
@@ -2,12 +2,14 @@ import create from "zustand"
 import { devtools, persist } from "zustand/middleware"
 import { produce } from "immer"
 
-export default accountStore = create(persist(devtools(
+const useAccountStore = create(persist(devtools(
     (set) => ({
         account : {},
 
         addAccount : (user) => set(produce((state) => {
-            state.account.email = user.email
+            state.account = user
         })),
     })
 ), {name:"account"}))
+
+export default useAccountStore
